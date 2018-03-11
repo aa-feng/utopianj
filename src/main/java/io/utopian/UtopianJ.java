4ecb2d9e35d6f1e5aa7aadae0688f69d8c99251b
aafeng <me@aafeng.com>
2018-03-11 23:43:06 +0100

    add method list

--- src/main/java/io/utopian/UtopianJ.java
+++ src/main/java/io/utopian/UtopianJ.java
@@ -1,11 +1,77 @@
 package io.utopian;
 
+import java.util.Map;
+
 /**
  * Utopian API Java Client
  *
  */
 public class UtopianJ {
 
+  public static String baseUrl = "https://api.utopian.io/api/";
 
+  /**
+   * return moderator lists as a JSON string
+   */
+  public static String getModerators() {
+
+    return null;
+  }
+
+  /**
+   * return sponsor list as a JSON string
+   */
+  public static String getSponsors() {
+
+    return null;
+  }  
+
+  /**
+   * check if an account is a moderator
+   */
+  public static boolean isModerator(String account) {
+
+    return false;
+  }
+
+  /**
+   * check if an account is a sponsor
+   */
+  public static boolean isSponsor(String account) {
+
+    return false;
+  }
+
+  /** 
+   * return Utopian posts determined by the query parameters
+   */
+  public static String getPosts(Map<String,String> params) {
+
+    return null;
+  }
+
+  /**
+   * return a post String 
+   */
+  public static String getPost(String author, String permlink) {
+
+    return null;
+  }
+
+  /**
+   * return the current status of utopian.io
+   */
+  public static String getStats() {
+
+    return null;
+  }
+
+  /**
+   * check if Bot is voting
+   */
+  public static boolean isVoting() {
+
+    return false;
+  }
 
 }
